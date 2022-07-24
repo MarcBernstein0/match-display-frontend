22fbc665976e2b3fd2e0eeb9b1ea8d2256d7be34
Marc Bernstein <marcbernstein98@gmail.com>
2022-07-23 20:54:02 -0400

    got typescript for material-ui component to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Matches } from './models/matches.interface';
-import { Match } from './api/api';
-import moment from 'moment';
-import DenseTable from './components/table';
+import CustomizedTables from './components/table';
 
 function App() {
-  const [matchResult, setMatches] = useState<Matches[]>([]);
-  const [isError, setIsError] = useState<boolean>(false);
-  
-  useEffect(() => {
-    const parsedDate = moment("2022-07-19", "YYYY-MM-DD");
-    Match.getMatches(parsedDate)
-      .then((data) => {
-        setMatches(data);
-      })
-      .catch((err) => {
-        setIsError(true);
-      });
-  }, []);
-
   return (
-    <DenseTable />
-    // <div className="App">
-    //   <ul className="Books">
-    //     {matchResult.map((matchMetaData) => (
-    //       <li key={matchMetaData.tournament_id}>
-    //         <h3>{matchMetaData.game_name}</h3>
-    //         {matchMetaData.match_list.map((match) => (
-    //           <p>{match.id} {match.player1_name} {match.player2_name} {match.round}</p>
-    //         ))}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
+    <CustomizedTables />
   );
 }
 
